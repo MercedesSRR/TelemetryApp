7346a8a160b7662bed424e8341f4e29f463309a6
Mercedes Senties <anamercedes.senties@gmail.com>
2023-03-15 14:53:28 -0700

    Firebase installation

---
+++ src/stores/remote/FirebaseCloudService.js
@@ -0,0 +1,53 @@
+/*Copyright (c) 2023. Mercedes Senties, All Rights Reserved*/
+
+import CloudService from "./CloudService";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+export default class FirebaseCloudService extends CloudService{
+    
+    constructor() {
+        super();
+
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        this.#_firebaseConfig = {
+            apiKey: "AIzaSyBBCEwk49eflEvOPqYPR1rOCRQUnFQKvM8",
+            authDomain: "finalproject-efd0d.firebaseapp.com",
+            projectId: "finalproject-efd0d",
+            storageBucket: "finalproject-efd0d.appspot.com",
+            messagingSenderId: "910021269817",
+            appId: "1:910021269817:web:8ef37514205f52dc6db0e8",
+            measurementId: "G-LDD8PM5MHS"
+        };
+
+        this.app = {}
+        this.analytics = {}
+        this.db = {}
+    }
+    
+    
+    connect() {
+        //overload to connect with remote service
+        // Initialize Firebase
+        this.app = initializeApp(this.#_firebaseConfig);
+        this.analytics = getAnalytics(this.app);
+        this.db = getFirestore(this.app)
+    }
+
+    save() {
+        //override to support CloudService save of record/struct
+    }
+
+    load() {
+        //overload to upload a single record back to the service
+    }
+
+    loadAll() {
+        //overload to use the CloudService to fetch a list of things
+    }
+}
---
+++ src/stores/telemetryStore.js
@@ -0,0 +1,60 @@
+/*
+Pinia Data Store.
+Copyright (c) 2023. Scott Henshaw, Kibble Game Studios Inc. All Rights Reserved.
+*/
+import { defineStore } from 'pinia'
+import { FirebaseCloudService } from './remote/FirebaseCloudService.js'
+
+const cloud = new FirebaseCloudService();
+cloud.connect();
+
+export const useTelemetryStore = defineStore('telemetryStore', {
+
+    state: () => ({
+        recordList: [{
+            row: 0,
+            col: 0,
+            playerAction: 0,
+            deltaTime: new Date().getMilliseconds(),
+            buildId: '0.0.0.1 build 1234'
+        },{
+            row: 2,
+            col: 2,
+            playerAction: 2,
+            deltaTime: new Date().getMilliseconds(),
+            buildId: '0.0.2.1 build 1235'
+        },{
+            row: 3,
+            col: 3,
+            playerAction: 3,
+            deltaTime: new Date().getMilliseconds(),
+            buildId: '0.0.3.1 build 1236'
+        },{
+            row: 4,
+            col: 4,
+            playerAction: 4,
+            deltaTime: new Date().getMilliseconds(),
+            buildId: '0.0.4.1 build 1237'
+        }]
+    }),
+
+    getters: {
+        trecList( state ) {
+            return state.recordList;
+        },
+
+        recordCount( state ) {
+            return state.recordList.length;
+        }
+    },
+
+    actions: {
+        addItem( anItem ) {
+            this.recordList.push( anItem )
+        },
+
+        fetch() {
+            //TODO: fetch all telemetry data from external source
+        }
+    }
+})
